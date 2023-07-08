46d34697c8a07362d8bafe4786378005819c7bed
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2023-07-08 15:51:30 -0500

    modificade task.js clent

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -7,7 +7,7 @@ export const getTasks = async () => await axios.get(TASKS_URL);
 
 export const getTask = async (id) => await axios.get(`${TASKS_URL}${id}`);
 
-export const createTask = async (newTask) => await axios.post(TASKS_URL, newTask);
+export const createTask = async (newTask) => await axios.post(`${TASKS_URL}`, newTask);
 
 export const updateTask = async (id, updatedTask) => await axios.put(`${TASKS_URL}${id}`, updatedTask);
 
