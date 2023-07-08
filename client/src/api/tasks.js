a9a82e75811963897e1648573c582972913e2850
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2023-07-08 16:17:03 -0500

    modificade task.js clent

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
-const TASKS_URL = `${URL}/api/tasks/`;
+const TASKS_URL = `${URL}/api/tasks`;
 
 export const getTasks = async () => await axios.get(TASKS_URL);
 
-export const getTask = async (id) => await axios.get(`${TASKS_URL}${id}`);
+export const getTask = async (id) => await axios.get(`${TASKS_URL}/${id}`);
 
-export const createTask = async (newTask) => await axios.post(`${TASKS_URL}`, newTask);
+export const createTask = async (newTask) => await axios.post(TASKS_URL, newTask);
 
-export const updateTask = async (id, updatedTask) => await axios.put(`${TASKS_URL}${id}`, updatedTask);
+export const updateTask = async (id, updatedTask) => await axios.put(`${TASKS_URL}/${id}`, updatedTask);
 
-export const deleteTask = async (id) => await axios.delete(`${TASKS_URL}${id}`);
+export const deleteTask = async (id) => await axios.delete(`${TASKS_URL}/${id}`);
